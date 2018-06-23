7656cdc2a4fbe5e33103a5fce145bbb18b161aa2
Tumlinh <tumlinh@protonmail.com>
2018-06-23 20:32:58 +0200

    Allow clients to login without this mod

--- src/main/java/claudy/Claudy.java
+++ src/main/java/claudy/Claudy.java
@@ -5,7 +5,7 @@ import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 
-@Mod(modid = Claudy.MODID, name = Claudy.NAME, version = Claudy.VERSION)
+@Mod(modid = Claudy.MODID, name = Claudy.NAME, version = Claudy.VERSION, acceptedMinecraftVersions = "[1.12,)", acceptableRemoteVersions = "*")
 public class Claudy
 {
     public static final String MODID = "claudy";
